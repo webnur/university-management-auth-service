5c80957f5bac6a9b889cbe288775e0955a459621
Nurul Islam <56204522+webnur@users.noreply.github.com>
2023-05-30 10:47:48 +0600

    cors install and setup done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-import express, { Application} from 'express';
+import express, { Application, Request, Response} from 'express';
+import cors from 'cors'
 const app: Application = express()
 
-app.get("/", (req , res) => {
-  res.send("Hello World!");
+app.use(cors());
+//parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.get("/", (req: Request , res: Response) => {
+  res.send("working successfully!");
 });
 
 export default app;
