7e85adf0ca90235236c2fb8a368f43e18af2f86e
Nurul Islam <56204522+webnur@users.noreply.github.com>
2023-06-17 15:33:48 +0600

    [done] user extra layer validation using zod

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,14 @@
 import { RequestHandler } from 'express'
 import { UserService } from './user.service'
-import { z } from 'zod'
+import { UserValidation } from './user.validation'
 
 const createdUser: RequestHandler = async (req, res, next) => {
   try {
     // request validation
     // body --> object
     // data --> object
-    const createUserZodSchema = z.object({
-      body: z.object({
-        role: z.string({ required_error: 'role is required' }),
-        password: z.string().optional(),
-      }),
-    })
-    await createUserZodSchema.parseAsync(req)
+
+    await UserValidation.createUserZodSchema.parseAsync(req)
     const { user } = req.body
     const result = await UserService.createUser(user)
     res.status(200).json({
